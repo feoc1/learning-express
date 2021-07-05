37171e08bd47f6dd6bcb6d43c037f0667eaf23f9
Felipe Castro <felipeoccastro@gmail.com>
2021-07-05 19:47:32 -0300

    Using the Express Router

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const members = require('./Members')
 const logger = require('./middleware/logger')
 
 const app = express()
@@ -12,29 +11,11 @@ const app = express()
 // Initialize middleware:
 // app.use(logger)
 
-// Gets all members:
-app.get('/api/members', (req, res) => res.json(members))
-
-// Get single member:
-app.get('/api/members/:id', (req, res) => {
-  const found = members.some(
-    member => member.id === parseInt(req.params.id)
-  )
-  if (found) {
-    res.json(
-      members.filter(
-        member => member.id === parseInt(req.params.id)
-      )
-    )
-  } else {
-    res
-      .status(400)
-      .json({ msg: `No member with the id of ${req.params.id}` })
-  }
-})
-
 // Set static folder:
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 5000
 
